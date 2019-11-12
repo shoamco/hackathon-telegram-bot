6d9c893be13384598e806eb60b638784f57bfedf
shoamco <shoamc98@gmail.com>
2019-11-12 14:30:50 +0200

    Driver: Extract the user's personal information from the telegram

--- user_driver_bot.py
+++ user_driver_bot.py
@@ -8,21 +8,26 @@ from telegram import ReplyKeyboardRemove
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler)
 
-
 import logging
 
-from telegram import Update,CallbackQuery
+from telegram import Update, CallbackQuery
 from telegram.ext import CommandHandler, CallbackContext
 import library_functions
+
 DRIVER_DATE, DRIVER_TIME, DRIVER_SOURCE, DRIVER_DESTINATION, DRIVER_PLACE = range(5)
 logger = logging.getLogger(__name__)
 
 
 def start_driver(update, context):
     chat_id = update.effective_chat.id
-    logger.info(f"in start_driver #{chat_id}")
+    first_name = update.effective_chat.first_name
+    last_name = update.effective_chat.last_name
+    username = update.effective_chat.username
+    logger.info(f"in start_driver id#{chat_id} name {first_name} {last_name} username {username}")
     update.message.reply_text('Enter a travel date\n', one_time_keyboard=True, reply_markup=buttons.get_dates_options())
-    context.user_data['driver'] = dict()
+    context.user_data['ride'] = dict()
+    context.user_data['user'] = {"id": chat_id, "last_name": last_name}
+    context.user_data['user'] = {"id": chat_id, "first_name": first_name, "last_name": last_name, "username": username}
     return DRIVER_DATE
 
 
@@ -35,8 +40,7 @@ def get_date(update: Update, context: CallbackContext):
     # chat_id = update.effective_chat.id
     logger.info(f"! [#{chat_id}] Callback {text!r}.  Checking age...")
     logger.info(f"in get_date #{chat_id}")
-    context.user_data['driver']['date'] = text
-
+    context.user_data['ride']['date'] = text
 
     context.bot.send_message(chat_id=chat_id, text='At what time do you want to leave?', reply_markup=None)
 
@@ -47,7 +51,7 @@ def get_time(update, context):
     text = update.message.text
     chat_id = update.effective_chat.id
     logger.info(f"in get_time #{chat_id}")
-    context.user_data['driver']['time'] = text
+    context.user_data['ride']['time'] = text
 
     update.message.reply_text('Enter place of departure:')
 
@@ -58,7 +62,7 @@ def get_source(update, context):
     text = update.message.text
     chat_id = update.effective_chat.id
     logger.info(f"in get_source #{chat_id}")
-    context.user_data['driver']['source'] = text
+    context.user_data['ride']['source'] = text
 
     update.message.reply_text('Enter a destination :')
 
@@ -69,7 +73,7 @@ def get_destination(update, context):
     text = update.message.text
     chat_id = update.effective_chat.id
     logger.info(f"in get_destination #{chat_id}")
-    context.user_data['driver']['destination'] = text
+    context.user_data['ride']['destination'] = text
 
     update.message.reply_text(' How many spare places do you have?')
     return DRIVER_PLACE
@@ -79,12 +83,13 @@ def get_place(update, context):
     text = update.message.text
     chat_id = update.effective_chat.id
     logger.info(f"in get_place #{chat_id}")
-    context.user_data['driver']['place'] = text
+    context.user_data['ride']['place'] = text
 
     update.message.reply_text(f"Your ride has been recorded:\n"
-                              f"{context.user_data['driver']}")
-    library_functions.insert_ride_to_db(context.user_data['driver'],chat_id )
-    # library_functions.insert_user()
+                              f"ride: {context.user_data['ride']}\n"
+                              f"user:{context.user_data['ride']} ")
+    library_functions.insert_ride_to_db(context.user_data['user'], chat_id)
+    library_functions.insert_user(context.user_data['user'])
     return ConversationHandler.END
 
 
